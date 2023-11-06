c37937e15649cbc30452ad1ef5d5c86b1ece38a0
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-11-05 20:50:40 -0500

    added to renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
- 
+  var licenses = [
+    ['Apache 2.0', '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'],
+    ['MIT', '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'],
+    ['ISC', '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'],
+    ['GNU GPL v3', '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)']
+  ]
+  for (var x = 0; x < licenses.length; x++) {
+    if (licenses[x][0] === license) {
+      return licenses[x][1];
+    }
+  }
+  
 }
 
 // TODO: Create a function that returns the license link
@@ -16,7 +27,8 @@ function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-
+  var badge = renderLicenseBadge(data.license);
+  console.log(badge)
   return `# ${data.title}
 
 `;
